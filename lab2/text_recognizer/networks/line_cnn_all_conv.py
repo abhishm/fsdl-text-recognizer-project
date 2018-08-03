28a2f13100785fcff9477f973e5c46d85a560b76
abhishm <abhishm@local>
2018-08-03 23:59:23 +0000

    lab 2 copy

--- lab2/text_recognizer/networks/line_cnn_all_conv.py
+++ lab2/text_recognizer/networks/line_cnn_all_conv.py
@@ -8,6 +8,21 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import Model as KerasModel
 
 
+import pathlib
+from typing import Tuple
+
+from boltons.cacheutils import cachedproperty
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D, Permute, Reshape, TimeDistributed, Lambda, ZeroPadding2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model as KerasModel
+
+from text_recognizer.models.line_model import LineModel
+from text_recognizer.networks.lenet import lenet
+from text_recognizer.networks.misc import slide_window
+
+
 def line_cnn_all_conv(
         input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
@@ -30,7 +45,18 @@ def line_cnn_all_conv(
     # remember that padding is set to 'valid' (by default) in the network above.
 
     ##### Your code below (Lab 2)
+    # (image_height // 2 - 2, image_width // 2 - 2, 64)
+    new_height = image_height // 2 - 2
+    new_width = image_width // 2 - 2
+    new_window_width = window_width // 2 - 2
+    new_window_stride = window_stride // 2
+    model.add(Conv2D(128, (new_height, new_window_width), (1, new_window_stride), activation='relu'))
+    model.add(Dropout(0.2))
+    # (1, num_windows, 128)
 
+    num_windows = int((new_width - new_window_width) / new_window_stride) + 1
+
+    model.add(Reshape((num_windows, 128, 1)))
     ##### Your code above (Lab 2)
     # (num_windows, 128, 1)
 
